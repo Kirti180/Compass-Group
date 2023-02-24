7789feb65fa309be329930ef07c601d94757bdd6
Kirti Shinde <kirti182001@gmail.com>
2023-02-24 21:49:04 +0530

    day-4

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -1,23 +1,29 @@
 function navbar(){
-    return`<nav id="nav">
-<i class="fa-solid fa-bars" id="hamburger"></i>
-<div id="name">
-<p id="compass" style="font-size: 30px; font-weight: 800;">Compass    Group</p>
-</div>
-<div id="other-comp">
-<p class="other"><a href="./register.html" class="decoretor">Espanol</a></p>
-<p class="other"><a href="./register.html" class="decoretor">List your property</a></p>
-<p class="other"><a href="./register.html" class="decoretor">Support</a></p>
-<p class="other"><a href="./register.html" class="decoretor">Trips</a></p>
-<p class="other"><a href="./register.html" class="decoretor">Sign in</a> </p>
-</div>
+    return`<nav>
+    <i class="fa-solid fa-bars" id="hamburger"></i>
+    <div id="nav_right_side">
+        <div id="brand_name"><a href="./index.html" class="indexstyle1" style="color:white; font-size: 30px; font-weight: 800; font-family: Alex Brush;">Compass Group</a> </div>
+        <!-- <input type="text" placeholder="Search here" > -->
+    </div>
+    <div id="nav_left_side">
+
+        <div style="text-decoration: none;"><span><a class="indexstyle" href="3l" style="color:white; text-decoration: none;">Espanol</a></span></div>
+
+        <div style="text-decoration: none;"><span><a href="#" class="indexstyle" style="color:white; text-decoration: none;">List your property</a> </span></div>
+        <div style="text-decoration: none;"><span><a href="#" class="indexstyle" style="color:white; text-decoration: none;">Support</a> </span></div>
+        <div> <span><a href="#" class="indexstyle" style="color:white; text-decoration: none;">Trips</a> </span></div>
+        <div style="text-decoration: none;"><span><a href="./register.html" class="indexstyle" style="color:white; text-decoration: none;">Sign in</a> </span></div>
+        <!-- <div><i class="fa-solid fa-cart-shopping "></i><span><a href="./cartpage.html" class="indexstyle">Support</a> </span></div> -->
+    </div>
 </nav>
-<div id="burger">
-<div>Espanol</div>
-<div>List your property</div>
-<div>Support</div>
-<div>Trips</div>
-<div>Sign in</div>
+
+
+<div id="burger" >
+    <div  class="burger_inside">Espanol/</div>
+    <div  class="burger_inside">List your property</div>
+    <div  class="burger_inside">Support</div>
+    <div  class="burger_inside">Trips</div>
+    <div   class="burger_inside"><a href="./register.html" > Sign in</a></div>
 </div>`
 }
 export default navbar
