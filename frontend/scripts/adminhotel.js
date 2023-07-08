0b451d6f7952a1eaf214e3ee663877319a61922c
Kirti Shinde <kirti182001@gmail.com>
2023-07-08 11:46:04 +0530

    updated code

--- frontend/scripts/adminhotel.js
+++ frontend/scripts/adminhotel.js
@@ -1,6 +1,6 @@
 let conatin = document.getElementById("conatin");
 const getdata = () => {
-    fetch("https://funny-newt-hat.cyclic.app/hotel/all")
+    fetch("https://bronze-katydid-gown.cyclic.app/hotel/all")
         .then((res) => res.json())
         .then((res) => {
             //    console.log(res)
@@ -86,7 +86,7 @@ function display(data) {
     });
 }
 async function handleDelete(id) {
-    await fetch(`https://funny-newt-hat.cyclic.app/hotel/delete/${id}`, {
+    await fetch(`https://bronze-katydid-gown.cyclic.app/hotel/delete/${id}`, {
       method: "DELETE",
       headers: {
         "Content-type": "application/json; charset=UTF-8",
